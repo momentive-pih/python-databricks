a12e09b9ce6a3d83ee1ab7d2024cb131ad3c2038
momentive-pih <61007983+momentive-pih@users.noreply.github.com>
2020-05-20 17:43:14 +0530

    Update momentive-pih-mount-dbfs-pythoncode.py

--- momentive-pih-mount-dbfs-pythoncode.py
+++ momentive-pih-mount-dbfs-pythoncode.py
@@ -1,15 +1,15 @@
 # Databricks notebook source
 dbutils.fs.mount(
   source = "wasbs://momentive-configuration@devstorpih001.blob.core.windows.net",
-  mount_point = "dbfs/mnt/momentive-configuration",
+  mount_point = "/mnt/momentive-configuration",
   extra_configs = {"fs.azure.account.key.devstorpih001.blob.core.windows.net":"hBTtfr+U72t9bsqx9dOBVZ7EiccMV5mcMXQDDPXH45yfTHIg0dOBuHDtwGjuxUP6Wc1zyBVaLws2Zda3AhuaYA=="})
 
 dbutils.fs.mount(
   source = "wasbs://momentive-pih-logging@devstorpih001.blob.core.windows.net",
-  mount_point = "dbfs/mnt/momentive-pih-logging",
+  mount_point = "/mnt/momentive-pih-logging",
   extra_configs = {"fs.azure.account.key.devstorpih001.blob.core.windows.net":"hBTtfr+U72t9bsqx9dOBVZ7EiccMV5mcMXQDDPXH45yfTHIg0dOBuHDtwGjuxUP6Wc1zyBVaLws2Zda3AhuaYA=="})
 
 dbutils.fs.mount(
   source = "wasbs://momentive-sources-pih@devstorpih001.blob.core.windows.net",
-  mount_point = "dbfs/mnt/momentive-sources-pih",
+  mount_point = "/mnt/momentive-sources-pih",
   extra_configs = {"fs.azure.account.key.devstorpih001.blob.core.windows.net":"hBTtfr+U72t9bsqx9dOBVZ7EiccMV5mcMXQDDPXH45yfTHIg0dOBuHDtwGjuxUP6Wc1zyBVaLws2Zda3AhuaYA=="})
