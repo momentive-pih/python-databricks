ee23286469d701e6c828bbf50b042f02214532c5
momentive-pih <61007983+momentive-pih@users.noreply.github.com>
2020-05-20 16:05:58 +0530

    Update momentive-pih-mount-dbfs-pythoncode.py

--- momentive-pih-mount-dbfs-pythoncode.py
+++ momentive-pih-mount-dbfs-pythoncode.py
@@ -10,6 +10,6 @@ dbutils.fs.mount(
   extra_configs = {"fs.azure.account.key.devstorpih001.blob.core.windows.net":"hBTtfr+U72t9bsqx9dOBVZ7EiccMV5mcMXQDDPXH45yfTHIg0dOBuHDtwGjuxUP6Wc1zyBVaLws2Zda3AhuaYA=="})
 
 dbutils.fs.mount(
-  source = "wasbs://momentive-source@devstorpih001.blob.core.windows.net",
-  mount_point = "dbfs/mnt/momentive-source",
+  source = "wasbs://momentive-sources-pih@devstorpih001.blob.core.windows.net",
+  mount_point = "dbfs/mnt/momentive-sources-pih",
   extra_configs = {"fs.azure.account.key.devstorpih001.blob.core.windows.net":"hBTtfr+U72t9bsqx9dOBVZ7EiccMV5mcMXQDDPXH45yfTHIg0dOBuHDtwGjuxUP6Wc1zyBVaLws2Zda3AhuaYA=="})
